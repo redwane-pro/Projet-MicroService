a1c7cc384354d15c5b15056501bb23c2debf13c1
redwane-pro <74648731+redwane-pro@users.noreply.github.com>
2021-03-30 11:28:42 +0200

    Add files via upload

---
+++ webservice4/src/test/java/com/example/webservice4/Webservice4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservice4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webservice4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
